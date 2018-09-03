04b0455eec087d72e1694294b9594d5616610745
Tebello Thejane <zyxoas@gmail.com>
2018-09-03 21:33:28 +0200

    Cache-Control headers should honestly not be baked into the API...

--- lib/Network/KunoMarket/API/Global.hs
+++ lib/Network/KunoMarket/API/Global.hs
@@ -9,14 +9,10 @@ import Network.KunoMarket.API.Global.Types
 import Data.Text
 
 type GlobalAPI =
-       "public" :> "symbols"
-                :> Get '[JSON] (Headers '[Header "Cache-Control" Text] [SymbolInfo])
-  :<|> "public" :> "symbols" :> Capture "symbol" Text
-                :> Get '[JSON] (Headers '[Header "Cache-Control" Text] SymbolInfo)
-  :<|> "public" :> "currencies"
-                :> Get '[JSON] (Headers '[Header "Cache-Control" Text] [CurrencyInfo])
-  :<|> "public" :> "currencies" :> Capture "currency" Text
-                :> Get '[JSON] (Headers '[Header "Cache-Control" Text] CurrencyInfo)
+       "public" :> "symbols"    :> Get '[JSON] [SymbolInfo]
+  :<|> "public" :> "symbols"    :> Capture "symbol" Text :> Get '[JSON] SymbolInfo
+  :<|> "public" :> "currencies" :> Get '[JSON] [CurrencyInfo]
+  :<|> "public" :> "currencies" :> Capture "currency" Text :> Get '[JSON] CurrencyInfo
   :<|> "public" :> "timestamp"  :> Get '[PlainText] Text
 
 globalApi :: Proxy GlobalAPI
